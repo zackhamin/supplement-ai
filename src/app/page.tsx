142f3834925bedfaee297244a2f414d3e56c7291
zackhamin <ishaqhamin@gmail.com>
2024-07-05 18:59:57 +0100

    chore(data): update title fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <h1 className="text-4xl font-bold mb-8">Hollistically</h1>
+        <h1 className="text-4xl font-bold mb-8">Holistically</h1>
         <div className="mb-6">
           <h2 className="text-xl font-semibold mb-2">
             Some example Questions. Remember, you can be really specific:
